32eee4c2b8b9a9035dffd35c3695d75de060818c
zhangrongwu <zhangrongwuios@icloud.com>
2018-08-02 15:03:25 +0800

    first commit

---
+++ WebView/JSObjectManager.h
@@ -0,0 +1,13 @@
+//
+//  JSObjectManager.h
+//  WebView
+//
+//  Created by zhangrongwu on 2018/8/2.
+//  Copyright © 2018年 ENN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JSObjectManager : NSObject
+
+@end
---
+++ WebView/JSObjectProtocol.h
@@ -0,0 +1,13 @@
+//
+//  JSObjectProtocol.h
+//  WebView
+//
+//  Created by zhangrongwu on 2018/8/2.
+//  Copyright © 2018年 ENN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol JSObjectProtocol <NSObject>
+
+@end
